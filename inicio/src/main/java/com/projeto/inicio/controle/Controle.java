ad61f696a36b3ae8a7a732459f9984e41268d76e
juan-pablo-git <salesjuan781@gmail.com>
2024-03-13 08:25:35 -0300

    novos metodos

--- inicio/src/main/java/com/projeto/inicio/controle/Controle.java
+++ inicio/src/main/java/com/projeto/inicio/controle/Controle.java
@@ -8,10 +8,13 @@ import com.projeto.inicio.repositorio.Repositorio;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class Controle {
@@ -19,36 +22,60 @@ public class Controle {
     @Autowired
     private Repositorio acao;
 
-    @PostMapping("/api")    
-    public Pessoa cadastrar(@RequestBody Pessoa obj){
+
+    @PutMapping("/api")
+    public Pessoa editar(@RequestBody Pessoa obj) {
+
         return acao.save(obj);
     }
 
+    @DeleteMapping("/api/{codigo}")
+    public void remover(@PathVariable int codigo) {
+        Pessoa obj = selecionarPeloCodigo(codigo);
+        acao.delete(obj);
+    }
+
+    @PostMapping("/pessoa")
+    public Pessoa pessoa(@RequestBody Pessoa p) {
+        return p;
+    }
+
+
+    @PostMapping("/api")
+    public Pessoa cadastrar(@RequestBody Pessoa obj) {
+        return acao.save(obj);
+    }
+
+    @GetMapping("/api/ordenarnomes")
+    public List<Pessoa> getMethodName() {
+        return acao.findByOrderByNome();
+    }
+    
+
     @GetMapping("/selecionar")
-    public List<Pessoa> selecionar(){
+    public List<Pessoa> selecionar() {
         return acao.findAll();
     }
 
+    @GetMapping("/api/contador")
+    public long contador() {
+        return acao.count();
+    }
+
     @GetMapping("/api/{codigo}")
-    public Pessoa selecionarPeloCodigo(@PathVariable int codigo){
+    public Pessoa selecionarPeloCodigo(@PathVariable int codigo) {
         return acao.findByCodigo(codigo);
     }
 
     @GetMapping("")
-    public String mensagem(){
+    public String mensagem() {
 
         return "Hello Word";
     }
 
     @GetMapping("/boasVindas/{nome}")
-    public String boasVindas( @PathVariable String nome){
-        return "Boas Vindas "+ nome;
+    public String boasVindas(@PathVariable String nome) {
+        return "Boas Vindas " + nome;
     }
-
-    @PostMapping("/pessoa")
-    public Pessoa pessoa(@RequestBody Pessoa p){
-        return p;
-    }
-
 
 }
